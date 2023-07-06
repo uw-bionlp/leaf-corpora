36b5b7924cd80b3432b76d9a32a8472f7f1c5f26
Nic Dobbins <ndobb@uw.edu>
2023-07-06 14:28:45 -0700

    updated

--- leaf_logical_forms/annotator_2/batch1/NCT03865875_13.js
+++ leaf_logical_forms/annotator_2/batch1/NCT03865875_13.js
@@ -2,7 +2,7 @@
 
 '-  Known history of the following pulmonary comorbidities : severe chronic obstructive pulmonary disease ( COPD ) defined as FEV 1 ≤ 50 % , emphysema , interstitial lung disease , and / or use of supplemental oxygen .'
 
-'-  Known eq(temporal_per(PAST)) of the following mod("pulmonary") cond("comorbidities") : severity(SEVERE) chronic() cond("obstructive pulmonary disease") ( cond() ) defined as lab("FEV 1") eq(op(LTEQ), val("50"), unit("%")) , cond("emphysema") , cond("interstitial lung disease") , and / or use of proc("supplemental oxygen") .'
+'-  Known eq(temporal_per(PAST)) of the following mod("pulmonary") cond("comorbidities") : severity(SEVERE) chronic() cond("obstructive pulmonary disease") ( cond("COPD") ) defined as lab("FEV 1") eq(op(LTEQ), val("50"), unit("%")) , cond("emphysema") , cond("interstitial lung disease") , and / or use of proc("supplemental oxygen") .'
 
 cond("comorbidities")
     .temporality(
@@ -15,7 +15,7 @@ cond("comorbidities")
                 .severity(SEVERE)
                 .chronic()
                 .equiv(
-                    cond()
+                    cond("COPD")
                 ),
             lab("FEV 1")
                 .num_filter(
